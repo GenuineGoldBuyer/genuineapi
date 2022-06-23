8dd1475a83189ef8cf1452500862481249c4ee70
sheikh-zafar <audios.shkzafar@gmail.com>
2022-06-23 21:54:16 +0400

    first commit

---
+++ products.js
@@ -0,0 +1,1186 @@
+export const items = [
+  {
+    id: 1,
+    items: 'Gold Cuban Link',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 2,
+    items: 'Diamond',
+    category: 'Bracelet',
+    description: '.',
+    img: 'AC_Condenser.png'
+  },
+  {
+    id: 3,
+    items: 'Gold Couples',
+    category: 'Bracelet',
+    description: '.',
+    img: 'AC_Condenser_Cooling_Fan.png'
+  },
+  {
+    id: 4,
+    items: 'Gold Tennis',
+    category: 'Bracelet',
+    description: '.',
+    img: 'AC_Controls.png'
+  },
+  {
+    id: 5,
+    items: 'Diamond Tennis',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 6,
+    items: 'Rose Gold',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 7,
+    items: 'Infinity',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 8,
+    items: 'Gold Rose Quartz',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 9,
+    items: 'White Gold',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 10,
+    items: 'Gold Cuff',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 11,
+    items: 'Gold and Amethyst',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 12,
+    items: 'Gold Butterfly',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 13,
+    items: 'Gold and Versace',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 14,
+    items: 'Gold Ring (Mehendi)',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 15,
+    items: 'Gold and Carnelian ',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 16,
+    items: 'Gold Heart',
+    category:"Bracelet",
+    description:
+      '.'
+  },
+  {
+    id: 17,
+    items: 'Gold and Emerald',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 18,
+    items: 'Clover',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 19,
+    items: 'Gold and Ruby',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 20,
+    items: 'Gold and Sapphire',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 21,
+    items: 'Gold and Tourmaline',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 22,
+    items: 'Gold Opal Tennis',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 23,
+    items: 'Gold Hematite',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 24,
+    items: 'Gold and Turquoise',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 25,
+    items: 'Gold and Citrine',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 26,
+    items: 'Moonstone Gold',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 27,
+    items: 'Gold Flower',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 28,
+    items: 'Gold Braided',
+    category: 'Bracelet',
+    description:
+      ''
+  },
+  {
+    id: 29,
+    items: 'Gold Arm Cuff',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 30,
+    items: 'Gold and Aquamarine',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 31,
+    items: 'Slave',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 32,
+    items: 'Gold Anchor',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 33,
+    items: 'Gold Link',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 34,
+    items: 'Gold Bolo',
+    category: 'Bracelet',
+    description:
+      '.'
+  },
+  {
+    id: 35,
+    items: 'Gold and Gemstone',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 36,
+    items: 'Gold Sunflower',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 37,
+    items: 'Malachite Clover',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 38,
+    items: 'Malachite',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 39,
+    items: 'Gold Garnet',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 40,
+    items: 'Gold Harry Potter',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 41,
+    items: 'Gold Lapis Lazuli',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 42,
+    items: 'Gold and Diamond',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 43,
+    items: 'Gold Amazonite',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 44,
+    items: 'Gold Boho Bracelet',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 45,
+    items: 'Gold and Black Beads',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 46,
+    items: 'Gold and Shungite',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 47,
+    items: 'Gold and Onyx',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 48,
+    items: 'Gold Cord',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 49,
+    items: 'Gold Stackable',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 50,
+    items: 'Plain Gold',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 51,
+    items: 'Gold Pyrite',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 52,
+    items: 'Gold and Topaz',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 53,
+    items: 'Gold Red Jasper',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 54,
+    items: 'Gold Vikings',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 55,
+    items: 'Diamond',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 56,
+    items: 'Gold Spiral',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 57,
+    items: 'Ring and Bracelet',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 58,
+    items: 'Caratlane Engagement or Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 59,
+    items: 'Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 60,
+    items: 'Rose Gold',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 61,
+    items: 'White Gold',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 62,
+    items: 'Gold and Tungsten',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 63,
+    items: 'White sapphire Engagement or Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 64,
+    items: 'Black Sapphire Engagement or Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 65,
+    items: 'Emerald Cut Engagement or Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 66,
+    items: 'Solitaire Engagement or Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 67,
+    items: 'Pear Shaped Diamond Engagement or Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 68,
+    items: 'Gold Ruby Engagement or Wedding',
+    category: '',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 69,
+    items: '1 Caret Diamond Enagement or Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 70,
+    items: '2 Caret Diamond Enagement or Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 71,
+    items: '3 Caret Diamond Enagement or Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 72,
+    items: '4 Caret Diamond Enagement or Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 73,
+    items: '5 Caret Diamond Enagement or Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 74,
+    items: 'Couples Silver',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 75,
+    items: 'Marquise Cut Diamond',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 76,
+    items: 'Pink Diamond',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 77,
+    items: 'Moisanite Diamond',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 78,
+    items: '3 Diamond Enagement or Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 79,
+    items: '10 Caret Diamond Enagement or Wedding',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 80,
+    items: '24k Gold',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 81,
+    items: '18k Gold',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 82,
+    items: '22k Gold',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 83,
+    items: '21k Gold',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 84,
+    items: 'Gold Spiral',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 85,
+    items: 'Full Finger Gold',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 86,
+    items: 'Gold Baby',
+    category: 'Ring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 87,
+    items: 'Kundan Kada',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 88,
+    items: 'Beaded Gold',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 89,
+    items: '22k Gold',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 90,
+    items: '24k Gold',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 91,
+    items: 'Gold Kada',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 92,
+    items: 'Gold with Red stone',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 93,
+    items: 'Silver Kada',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 94,
+    items: 'Rose Gold Kada',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 95,
+    items: 'Rajwadi Kada',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 96,
+    items: 'Chakradhari Brass Kada',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 97,
+    items: 'Ghungroo Kada',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 98,
+    items: '2 Tola Gold Kada',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 99,
+    items: 'Gold Polki',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 100,
+    items: '18k Gold',
+    category: 'Bangles',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 101,
+    items: 'Gold Hoop',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 102,
+    items: 'Diamond Hoop',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 103,
+    items: 'Sui Dhaga',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 104,
+    items: 'Diamond Stud',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 105,
+    items: '1 Carat Diamond',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 106,
+    items: '2 Caret Diamond',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 107,
+    items: '3 Caret Diamond',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 108,
+    items: '4 Caret Diamond',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 109,
+    items: '5 Caret Diamond',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 110,
+    items: '10 Caret Diamond',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 111,
+    items: 'Rose Gold',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 112,
+    items: 'White Gold',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 113,
+    items: 'Solitaire Gold',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 114,
+    items: 'Gold Chandbali',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 115,
+    items: 'Gold Emerald',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 116,
+    items: 'Gold Chunky style',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 117,
+    items: 'Gold Huggies',
+    category:"Earring",
+    description:
+      '.'
+  },
+  {
+    id: 118,
+    items: 'Jimiki Kamal',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 119,
+    items: 'Jhumkas',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 120,
+    items: 'Tanzanite Earring',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 121,
+    items: '18k Gold',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 122,
+    items: '22k Gold',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 123,
+    items: '24k Gold',
+    category: 'Earring',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 124,
+    items: '18k Gold',
+    category: 'Chain',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 125,
+    items: '22k Gold',
+    category: 'Chain',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 126,
+    items: '24k Gold',
+    category: 'Chain',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 127,
+    items: 'Gold Hollow Rope',
+    category: 'Chain',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 128,
+    items: 'Layered Gold',
+    category: 'Chain',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 129,
+    items: 'Tennis Rope',
+    category: 'Chain',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 130,
+    items: '10k Gold',
+    category: 'Chain',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 131,
+    items: '14k Gold',
+    category: 'Chain',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 132,
+    items: 'Twisted Rope Gold',
+    category: 'Chain',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 133,
+    items: '3 Layer Gold',
+    category: 'Chain',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 134,
+    items: '2 Layer Gold',
+    category: 'Chain',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 135,
+    items: 'Kundan Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 136,
+    items: 'Collar Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 137,
+    items: 'Polki Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 138,
+    items: 'Choker Gold',
+    category: '',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 139,
+    items: 'Kemp Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 140,
+    items: 'Lariat',
+    category: '',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 141,
+    items: 'Choker Set',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 142,
+    items: 'Women Tentacion Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 143,
+    items: 'Pearl Choker',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 144,
+    items: 'Diamond Choker',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 145,
+    items: 'Black Choker',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 146,
+    items: 'Layered',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 147,
+    items: 'Multi Layered Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 148,
+    items: 'Rose Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 149,
+    items: 'White Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 150,
+    items: 'Gold Polki Sets',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 151,
+    items: 'Gold Emerald',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 152,
+    items: 'Bridal Choker',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 153,
+    items: 'Traditional Pearl',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 154,
+    items: 'Gemstone, Uncut diamond, Emerald, Rubies, Pearls Jadau Set',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 155,
+    items: 'Gold Moti Choker',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 156,
+    items: 'Rajasthani Gold Choker',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 157,
+    items: 'Triple Layered Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 158,
+    items: 'Small Pearl Choker Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 159,
+    items: 'Simple Gold Choker',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 160,
+    items: 'Bohemian Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 161,
+    items: 'Rope Choker',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 162,
+    items: 'CaratLane Choker',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 163,
+    items: 'Butterfly Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 164,
+    items: 'Ghungroo Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 165,
+    items: 'Uncut Diamond Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 166,
+    items: 'Floral Gold Choker',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 167,
+    items: 'Monogram Gold',
+    category: 'Necklace',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 168,
+    items: 'Gold Anklet',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+  {
+    id: 169,
+    items: 'Silver Anklet',
+    category: 'Bracelet',
+    description: '.',
+    img: ''
+  },
+]
+export default items;
